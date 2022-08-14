4a2a92ed93b78ebe51016de62b6c4505a0c7daa4
Oki John <blackpopex@yahoo.com>
2022-08-14 02:04:53 +0100

    mobile side menu bar and search container for mobile to desktop has been fixed

--- js/main.js
+++ js/main.js
@@ -14,3 +14,26 @@ for (i = 0; i < accordion.length; i++) {
     }
   });
 }
+
+//TOGGLE OPEN AND CLOSING OF SIDEBAR ON MOBILE
+var openSidebar = document.querySelector("#navToggle");
+var closeBtn = document.querySelector("#closeSidebar");
+
+var mobileMenu = document.querySelector("#menu_bar");
+var getOurApp = document.querySelector("#getApp");
+var searchBTN = document.querySelector("#SearchBTN");
+var sectionContainer = document.querySelector("#sectionContainer");
+
+openSidebar.addEventListener("click", function () {
+  mobileMenu.style.width = "100%";
+  getOurApp.classList.add("opacity_visible");
+});
+
+closeBtn.addEventListener("click", function () {
+  mobileMenu.style.width = "0";
+  getOurApp.classList.remove("opacity_visible");
+});
+
+searchBTN.addEventListener("click", function () {
+  sectionContainer.classList.toggle("showSearch");
+});
