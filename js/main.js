e3feab26b2aea82ce88eb307b4aef9294bc5394e
Oki John <blackpopex@yahoo.com>
2022-08-14 13:40:21 +0100

    Back to the top BTN fixed

--- js/main.js
+++ js/main.js
@@ -53,3 +53,29 @@ closeContactBtn.addEventListener("click", function () {
   contactForm.classList.remove("toggle-width");
   contactForm.style.right = "-60px";
 });
+
+//Take page back to the top
+window.onscroll = function () {
+  //Declared the scrollfunction() to handle hide and show back to top button;
+  scrollFunction();
+};
+
+var topBtn = document.getElementById("myTopBTN");
+var topBtnText = document.getElementById("BackTopBtn");
+function scrollFunction() {
+  if (
+    document.documentElement.scrollTop > 200 ||
+    document.body.scrollTop > 200
+  ) {
+    topBtn.style.display = "block";
+    topBtnText.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+    topBtnText.style.display = "none";
+  }
+}
+//This function takes the webpage back to the top
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
